454060f2004f08307916f94202d028cf39fb0f12
Mostafa Abuelabbas <mostafa.abuelabbas@gmail.com>
2025-11-11 09:54:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,30 +8,48 @@
 
 using namespace std;
 
-// ==================== Person Class Implementation =========================
 
-
-
-
-
-
+    void Person::display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"ID: "<<id<<endl;
+    }
 
 
 // ==================== Student Class Implementation ====================
 
-
-
-
+    void Student::display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"ID: "<<id<<endl;
+        cout<<"YearLevel: "<<yearLevel<<endl;
+        cout<<"major: "<<major<<endl;
+    }
 
 
 // ==================== Instructor Class Implementation ====================
 
 
-
-
+    void Instructor::display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"ID: "<<id<<endl;
+        cout<<"departemnt: "<<department<<endl;
+        cout<<"ExperienceYears: "<<experienceYears<<endl;
+    }
 
 
 // ==================== Course Class Implementation ====================
+
+    void Course::addStudent(const Student& s){
+        if(currentStudents<maxStudents)
+            students[currentStudents]=s;
+        else 
+            cout<<"The array is full"<<endl;
+    }
+    void Course::displayCourseInfo(){
+        cout<<courseCode<<"-"<<courseName<<endl;
+        cout<<"Max students: "<<maxStudents<<endl;
+        cout<<"Current enrolled: "<<currentStudents<<endl;
+        
+    }
 
 
 
@@ -42,7 +60,12 @@ using namespace std;
 
 // ==================== Main Function ====================
 int main() {
-   
-    
+    Student s(mostafa,cs,2);
+   s.display();
+    Course c(1122,cs,23,s,2);
+   c.display();
+   c.addStudent(s);    
+    Instructor i(moss,cs,2);
+   i.display();
     return 0;
 }
